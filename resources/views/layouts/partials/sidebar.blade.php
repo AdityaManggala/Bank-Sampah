9bb45bc6fa05f844d3227112d6576b4932e14464
AdityaManggala <aditya.manggala12345@gmail.com>
2022-09-17 13:07:39 +0700

    fix sidebar, grouping transaksi nasabah & pengepul

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{ route('dashboard') }}" class="brand-link">
         <img src="{{ asset('') }}dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
@@ -24,7 +24,7 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
+                data-accordion="true">
                 <!-- Add icons to the links using the .nav-icon class
                 with font-awesome or any other icon font library -->
 
@@ -36,7 +36,6 @@
                 <i class="nav-icon fas fa-th"></i>
                 <p>
                     Dashboard
-                    {{-- <span class="right badge badge-danger">New</span> --}}
                 </p>
                 </a>
             </li>
@@ -47,54 +46,46 @@
                 <i class="nav-icon fas fa-dumpster"></i>
                 <p>
                     Saldo Sampah
-                    {{-- <span class="right badge badge-danger">New</span> --}}
                 </p>
                 </a>
             </li>
             <li class="nav-item">
-                <a href="{{ route('transaksi-pengepul.index') }}" class="nav-link @if (Route::is('transaksi-pengepul.index'))
-                                    active @endif">
-                                    {{-- <i class=""></i> --}}
-                                    {{-- <i class="far fa-money-bill"></i> --}}
-                <i class="nav-icon fas fa-money-bill"></i>
-                <p>
-                    Transaksi Pengepul
-                    {{-- <span class="right badge badge-danger">New</span> --}}
-                </p>
-                </a>
-            </li>
-                <li class="nav-item">
                 <a href="#" class="nav-link">
-                {{-- <i class="nav-icon fas fa-tachometer-alt"></i> --}}
-                <i class="nav-icon fas fa-sitemap"></i>
-                <p>
-                    Data Master
-                    <i class="right fas fa-angle-left"></i>
-                </p>
+                    <i class="nav-icon fas fa-dollar-sign"></i>
+                    <p>
+                        Transaksi
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
                 </a>
                 <ul class="nav nav-treeview">
-
                     <li class="nav-item">
-                        <a href="{{ route('dashboard') }}"
-                            class="nav-link @if (Route::is('dashboard')) active @endif">
-                            <i class="nav-icon fas fa-th"></i>
-                            <p>
-                                Dashboard
-                                <span class="right badge badge-danger">New</span>
-                            </p>
+                        <a href="{{ route('transaksi-pengepul.index') }}" class="nav-link @if (Route::is('transaksi-pengepul.index'))
+                                    active @endif">
+                            <i class="far fa-circle nav-icon "></i>
+                            <p>Transaksi Pengepul</p>
                         </a>
-                    @elseif (Auth::guard('nasabah')->check())
+                    </li>
                     <li class="nav-item">
-                        <a href="{{ route('nasabah.profilNasabah') }}"
-                            class="nav-link @if (Route::is('nasabah.profilNasabah')) active @endif">
-                            <i class="nav-icon fa fa-user"></i>
-                            <p> Profil Nasabah </p>
+                        <a href="{{ route('transaksi-nasabah.index') }}" class="nav-link @if (Route::is('transaksi-nasabah.index'))
+                            active @endif">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Transaksi Nasabah</p>
                         </a>
+                    </li>
+                </ul>
+            </li>
+                @elseif (Auth::guard('nasabah')->check())
+                <li class="nav-item">
+                    <a href="{{ route('nasabah.profilNasabah') }}"
+                        class="nav-link @if (Route::is('nasabah.profilNasabah')) active @endif">
+                        <i class="nav-icon fa fa-user"></i>
+                        <p> Profil Nasabah </p>
+                    </a>
+                </li>
                 @endif
                 @if (Auth::guard('admin')->check())
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-                            {{-- <i class="nav-icon fas fa-tachometer-alt"></i> --}}
                             <i class="nav-icon fas fa-sitemap"></i>
                             <p>
                                 Data Master
@@ -130,13 +121,6 @@
                                     <p>Nasabah</p>
                                 </a>
                             </li>
-                            <li class="nav-item">
-                                <a href="{{ route('transaksi-nasabah.index') }}"
-                                    class="nav-link @if (Route::is('transaksi-nasabah.index')) active @endif">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Transaksi Nasabah</p>
-                                </a>
-                            </li>
                         </ul>
                     </li>
                     @elseif (Auth::guard('nasabah')->check())
@@ -144,30 +128,10 @@
                         <a href="{{ route('transaksi.nasabah', Auth::id()) }}"
                             class="nav-link @if (Route::is('transaksi.nasabah')) active @endif">
                             <i class="fas fa-shopping-cart nav-icon"></i>
-                            
                             <p>Transaksi Nasabah</p>
                         </a>
                     </li>
-                @endif
-                </li>
-                    <li class="nav-item">
-                        <a href="{{ route('transaksi-nasabah.index') }}" class="nav-link @if (Route::is('transaksi-nasabah.index'))
-                                    active
-                                @endif">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Transaksi Nasabah</p>
-                        </a>
-                    </li>
-                    {{-- <li class="nav-item">
-                        <a href="{{ route('transaksi-pengepul.index') }}" class="nav-link @if (Route::is('transaksi-pengepul.index'))
-                                    active
-                                @endif">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Transaksi Pengepul</p>
-                        </a>
-                    </li> --}}
-                </ul>
-            </li>
+                @endif                
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
